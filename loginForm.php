cf1adba83b5ce9265500270312ee088e0a29e4a9
IT21169380 <isurusanka98@gmail.com>
2022-05-22 00:04:12 +0530

    resolve changes

--- loginForm.php
+++ loginForm.php
@@ -161,7 +161,7 @@ if (isset($_POST['submit'])) {
                         }
                         ?>
                         <label for="uname"><b>Username</b></label>
-                        <input type="text" placeholder="Enter Username" name="uname" required/>
+                        <input type="text" placeholder="Enter Your E-mail as Username" name="uname" required/>
 
                         <label for="psw"><b>Password</b></label>
                         <input type="password" placeholder="Enter Password" name="psw" required/>
